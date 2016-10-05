0233000b8220888ccdb4017f412c9a9ab34fbc2d
Nikolas Bompetsis <nikolas.mpompetsis@gmail.com>
2016-10-05 00:04:04 +0000

    Critical changes @ route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'dashboard/index'
+
   resources :users
   root 'welcome_page#index'
-  # get 'sessions/login'
+  get 'sessions/login'
   match 'login', to:  'sessions#login', via: :all
-
   # get 'welcome_page/index'
   get '/index',  to: 'welcome_page#index'
+  get '/logout',  to: 'sessions#logout'
   # get '/profile', to: 'sessions#login'
-  get 'users/signin'
-  # match 'users/signin', to: 'devise/users#signin', via: :post
-  # get  '/signin',    to: 'users#signup'
-  # Now, we can the navigate to /users/<id>
 
 end
